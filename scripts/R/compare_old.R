dbe626b8840434ea2a422487d4d70194a75788e6
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2020-07-22 22:05:15 -0700

    ok

--- scripts/R/compare_old.R
+++ scripts/R/compare_old.R
@@ -1,39 +1,89 @@
+this <- system('hostname', TRUE)
+if (this == "LAPTOP-IVSPBGCA") { dp <- "C:/github/diets/scripts"
+} else { dp <- "C:/Users/jccaro/diets/scripts"}
 
-library(gtdq)
+
 library(diets)
-#path <- file.path(system.file(package = "gtdq"), "external")
-#old_content <- base::readRDS(file.path(path, "example/Content.RDS"))
-#old_consump <- base::readRDS(file.path(path, "example/Consumption.RDS"))
-#old_intake  <- gtdq::Intake(old_consump, old_content, "ENERGY", Wgpath=NULL, zn=TRUE)
-#old_adj_intake <- gtdq::AdjustIntake("RWA", old_intake, ca=TRUE, fe=TRUE, fortif=FALSE)
 
-new_content <- nutrientContent(continent="Africa", redpalmoil=0.5, orangesweetpot=0)
-old_content <- new_content[, c(1,3,4)]
-colnames(old_content) <- c("Code_FdGp1", "Tagname", "MNutr_Val")
+##################
+## compute intake for one country
+##################
 
-consumption <- readRDS(system.file("ex/rwa_FBS.rds", package = "diets"))
-u <- unique(new_content[,1:2])
-m <- merge(consumption, u, by.x="Item", by.y="group", all.x=TRUE)
-m[is.na(m$code), ]
-consumption <- m[!is.na(m$code), ]
+country_intake <- function(country, region, fortify=TRUE, adjust=TRUE) {
 
-consumption <- consumption[consumption$Element == "Food supply (kcal/capita/day)", c("Item", "Value", "code")]
-new_consumption <- consumption[,1:2]
-colnames(new_consumption) <- c("group", "value")
-old_consumption <- consumption[,3:2]
-colnames(old_consumption) <- c("Code_FdGp1", "FdGp1_Val")
+	print(country); flush.console()
+	cons <- consumption[consumption$country == country, ]
+	cont <- content[[region]]
+	if (fortify) {
+		fort_country <- fort[fort$area == country, ]
+		cont <- fortify(cont, fort_country)
+	}
+	
+	years <- unique(cons$year)
+	yout <- list()
+	for (j in 1:length(years)) {
+		year_cons <- cons[cons$year == years[j],  c("group", "value")]
+		intake <- nutrientIntake(year_cons, cont, verbose=FALSE)
+		if (adjust) {
+			intake <- add_Ca(intake)
+			intake <- adjust_Ca(intake)
+			intake <- adjust_Fe(intake, Nheme)
+			intake <- adjust_Zn(intake)
+		}
+		intake <- aggregate(intake[, "intake", drop=FALSE], intake[, c("tag", "unit", "desc")], sum, na.rm=TRUE)
+		intake$year <- years[j]
+		yout[[j]] <- intake
+	}
+	cntr_intake <- do.call(rbind, yout)
+	data.frame(country=country, region=region, cntr_intake)		
+}
 
-old_intake  <- gtdq::Intake(old_consumption, old_content, "ENERGY", Wgpath=NULL, zn=FALSE)
-new_intake  <- nutrientIntake(new_consumption, new_content, verbose=FALSE)
+##################
+## Load data for all countries
+##################
+pop <- readRDS(file.path(dp, "WPP2019_PopulationBySingleAgeSex_1950-2019.rds"))
+consumption <- readRDS(file.path(dp, "FBS.rds"))
+consumption <- consumption[consumption$Element == "Food supply (kcal/capita/day)", c("ISO3", "Year", "Item", "Value")]
+colnames(consumption) <- c("country", "year", "group", "value")
+country_reg <- readRDS(file.path(dp, "data/countries.rds"))[, c("ISO3","CONTINENT")]
+colnames(country_reg)[2] <- "continent"
+Nheme  <- readRDS(system.file("ex", "NHemeIron.rds", package="diets"))
+fort <- readRDS(system.file("ex/fortification.rds", package="diets"))
+conts <- c("Africa", "Americas", "Asia", "Europe", "Oceania")
+content <- lapply(conts, function(i) nutrientContent(continent=i, redpalmoil=0.5, orangesweetpot=0.2))
+names(content) <- conts 
+countries <- unique(consumption$country)
+countries <- merge(data.frame(ISO3=countries), country_reg, by="ISO3")
 
-old <- aggregate(old_intake[,"MNutr_PersonDay",drop=FALSE],  old_intake[,"Tagname",drop=FALSE], sum, na.rm=TRUE)
-old[old[,1] =="FE", ]
-new <- aggregate(new_intake[,"intake",drop=FALSE], new_intake[ ,"tag",drop=FALSE], sum)
-new[new[,1] =="FE", ]
+### old results for comparison
+old <- read.csv(file.path(dp, "data/2017paper_data.csv"))
+old <- old[, c("ISO3", "Year", "Fortification", "Tagname", "Units", "Intake")]
 
-# intake is good
-x <- merge(old, new, by=1)
-plot(x[,2:3])
+
+### pick your country from "countries"
+i <- 1
+x <- country_intake(countries[i,1], countries[i,2], fortify=T, adjust=T)
+
+m <- merge(old, x, by.x=c("ISO3", "Year", "Tagname"), by.y=c("country", "year", "tag"))
+# difference as %
+m$delta <- 100 * ((m$Intake - m$intake) / m$Intake)
+
+# pick a year
+year <- 1961 
+y <- m[m$Year %in% year, c("Tagname", "Intake", "intake", "delta")]
+y[,2:3] <- round(y[,2:3], 5)
+y <- y[rev(order(y[,2])), ]
+y
+
+# plot all years
+tags <- y$Tagname
+z <- m[,c("Tagname", "Intake", "intake")]
+i <- match(z[,1], tags)
+cols <- rainbow(nrow(y))
+plot(z[,2:3], col=cols[i])
 abline(0,1)
+legend("topleft", legend=tags[1:4], col=cols[1:4], pch=1)
 
-
+# average differences over all years.
+a <- tapply(m$delta, m$Tagname, mean)
+sort(round(a, 3))
